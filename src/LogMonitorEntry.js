5c531e0b2c75e439278d8513675bd25a961501ef
Kyriakos Ziakoulis <cooxlee@gmail.com>
2017-04-10 14:24:51 +0300

    Fix PropTypes deprecation warnings react >= 15.5.0

--- src/LogMonitorEntry.js
+++ src/LogMonitorEntry.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import JSONTree from 'react-json-tree';
 import LogMonitorEntryAction from './LogMonitorEntryAction';
 import shouldPureComponentUpdate from 'react-pure-render/function';
